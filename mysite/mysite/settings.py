edc3ce70909b0564f556e84e1a63a57d78abe9f1
Michael Pinna <mikepinna3@gmail.com>
2021-07-23 20:49:09 -0400

    Added django.shortcuts render and get_object_or_404()

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+# Look into DJANGO_SETTINGS_MODULE
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
